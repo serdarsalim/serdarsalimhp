c74d6233b6f3997d660ac0cf07fa2e09030eafea
SLM <slm@SLM-AIR.local>
2025-12-10 21:50:27 +0800

    removed first cloud in about me section

--- app/components/SkyDay.tsx
+++ app/components/SkyDay.tsx
@@ -6,7 +6,6 @@ export default function SkyDay() {
   return (
     <div className={styles.sky}>
       <div className={styles.moon}></div>
-      <div className={styles.cloud}></div>
       <div className={`${styles.cloud} ${styles['cloud-1']}`}></div>
       <div className={`${styles.cloud} ${styles['cloud-2']}`}></div>
       <div className={`${styles.cloud} ${styles['cloud-3']}`}></div>
